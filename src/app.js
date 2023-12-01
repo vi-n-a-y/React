e1de62408a648ae262891be336c43c1441e47ace
vi-n-a-y <balavinay9063@gmail.com>
2023-12-01 13:34:05 +0530

    react

---
+++ src/app.js
@@ -0,0 +1,9 @@
+var a=document.createElement("h1");
+a.innerText = "Hello world javaScript!...";
+var b=document.getElementById("root");
+b.appendChild(a);
+
+let world=React.createElement("h1",{},"Hello React");
+let get=document.getElementById("root1");
+let data=ReactDOM.createElement(hw);
+data.render(world);
